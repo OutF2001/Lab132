fd11824a7bd0531c25e6df0c8af5dda2cbd2f323
OutF <stefanovaldeos@gmail.com>
2024-12-07 10:34:38 -0500

    Initial commit

---
+++ app/src/main/java/com/valdeos/flavio/lab132/GalleryActivity.kt
@@ -0,0 +1,22 @@
+package com.valdeos.flavio.lab132
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.valdeos.flavio.lab132.databinding.ActivityGalleryBinding
+import java.io.File
+
+class GalleryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGalleryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGalleryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val directory = File(externalMediaDirs[0].absolutePath)
+        val files = directory.listFiles() as Array<File>
+
+        val adapter = GalleryAdapter(files.reversedArray())
+        binding.viewPager.adapter = adapter
+    }
+
+}
